886e5f35c33bb9118df3a0e70f85ef8341722db7
hongten <m10117013@gmail.com>
2019-02-27 10:51:55 +0800

    update signaling server

--- signaling/app.js
+++ signaling/app.js
@@ -4,7 +4,7 @@ const wss = new WebSocket.Server({ port: 8080 }, () => {
     console.log("Signaling server is now listening on port 8080")
 });
 
-// Broadcast to all.
+// Broadcast to all exclude self.
 wss.broadcast = (ws, data) => {
     wss.clients.forEach((client) => {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -13,11 +13,18 @@ wss.broadcast = (ws, data) => {
     });
 };
 
+// Broadcast to all 
+wss.broadcastAll = (ws, data) => {
+    wss.clients.forEach((client) => {
+            client.send(data);
+    });
+};
+
 wss.on('connection', (ws) => {
     console.log(`Client connected. Total connected clients: ${wss.clients.size}`)
     
     if (wss.clients.size > 1) {
-	    wss.broadcast(ws, "{\"t\":\"r\"}");
+	    wss.broadcastAll(ws, "{\"t\":\"r\"}");
     }
     
     ws.onmessage = (message) => {
